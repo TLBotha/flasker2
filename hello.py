28875f29cf90c2ad56e2251c5c640d103bb52be0
Theunis Botha <tlbotha@hotmail.com>
2022-07-06 15:43:24 +0200

    Flask Friday#7 update

--- hello.py
+++ hello.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "super secret key"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Create a route decorator
 @app.route('/')
@@ -45,6 +56,21 @@ def user(name):
 def page_not_found(e):
     return render_template("404.html"), 404
 
+# Internal Server Error
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully!")
+    return render_template("name.html",
+        name = name,
+        form = form)
